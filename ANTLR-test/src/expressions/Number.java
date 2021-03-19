8916a12a9cfddc459a8cf1732cc7a958b437dd45
NanaSandberg <nanagsandberg@gmail.com>
2021-03-19 07:29:03 +0100

    added Number int + toString
    Number is represented by int for now.
    
    Added toString method.

--- ANTLR-test/src/expressions/Number.java
+++ ANTLR-test/src/expressions/Number.java
@@ -1,5 +1,15 @@
 package expressions;
 
 public class Number extends Expression {
+    public int num;
+
+    public Number(int num){
+        this.num = num;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(num);
+    }
 
 }
