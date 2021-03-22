b6f09faea367de542e15eb0e56e7374a9f9bcb3a
adamstuck <adam.moloney.stuck@gmail.com>
2021-03-22 19:52:17 +0100

    VisitNumber + VisitVariableDeclaration + VisitVariable

--- ANTLR-test/src/models/expressions/AntlrToExpression.java
+++ ANTLR-test/src/models/expressions/AntlrToExpression.java
@@ -14,6 +14,9 @@ import antlr.expressionParser.VariableContext;
 
 public class AntlrToExpression extends expressionBaseVisitor<Expression> {
 
+	private List<String> vars; 
+	private List<String> semanticErrors; 
+	
 	@Override
 	public Expression visitDeclaringReaction(DeclaringReactionContext ctx) {
 		// TODO Auto-generated method stub
@@ -23,8 +26,21 @@ public class AntlrToExpression extends expressionBaseVisitor<Expression> {
 	//DECLARATION
 	@Override
 	public Expression visitDeclaringInt(DeclaringIntContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitDeclaringInt(ctx);
+		Token idToken = ctx.ID().getSymbol();
+		int line = idToken.getLine();
+		int column = idToken.getCharPositionInLine() + 1;
+		
+		String id = ctx.getChild(0).getText();
+		if (vars.contains(id)) {
+			semanticErrors.add("Error @" + line.toString() + "," + column.toString() + " : variable '" + id + "' already declared.")
+		} else {
+			vars.add(id);
+		}
+		
+		String type = ctx.getChild(2).getText();
+		int value = Integer.parseInt(ctx.NUM().getText());
+		
+		return new VariableDeclaration(id, type, value);
 	}
 	
 	//REACTION OPERATOR
@@ -51,15 +67,24 @@ public class AntlrToExpression extends expressionBaseVisitor<Expression> {
 	//NUMBER
 	@Override
 	public Expression visitNumber(NumberContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitNumber(ctx);
+		String numText = ctx.getChild(0).getText();
+		int num = Integer.parseInt(numText);
+		return new Number(num);
 	}
 
 	//Variable
 	@Override
 	public Expression visitVariable(VariableContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitVariable(ctx);
+		Token idToken = ctx.ID().getSymbol();
+		int line = idToken.getLine();
+		int column = idToken.getCharPositionInLine() + 1;
+		
+		String id = ctx.getChild(0).getText();
+		if (!vars.contains(id)) {
+			semanticErrors.add("Error @" + line.toString() + "," + column.toString() + " : variable '" + id + "' not declared.")
+		}
+		
+		return new Variable(id);
 	}
 	
 	/*@Override
