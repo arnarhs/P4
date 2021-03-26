c39e8db4587854427baa467bbfbbddf58f1cfe4b
NanaSandberg <nanagsandberg@gmail.com>
2021-03-26 11:37:25 +0100

    Changed Program to contain Statements
    Changed component classes on program to instead contain a List<Statementent> instead of List<Expression>

--- ANTLR-test/src/models/Program.java
+++ ANTLR-test/src/models/Program.java
@@ -3,17 +3,15 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 
-import models.expressions.Expression;
-
 public class Program {
-	public List<Expression> expressions;
+	public List<Statement> statements;
 	
 	public Program() {
-		this.expressions = new ArrayList<>();
+		this.statements = new ArrayList<>();
 	}
 	
-	public void AddExpression(Expression expr) {
-		expressions.add(expr);
+	public void AddExpression(Statement stmt) {
+		statements.add(stmt);
 	}
 
 }
